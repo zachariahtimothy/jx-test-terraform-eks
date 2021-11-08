f343ca10753f4eb19339ddc3e1e05f2689cb30ec
Zach Curtis <zachariahtimothy@gmail.com>
2021-11-08 09:34:13 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,7 @@
 variable "region" {
   description = "AWS region code for creating resources."
   type        = string
+  default     = us-east-1
 }
 
 variable "profile" {
@@ -14,6 +15,7 @@ variable "profile" {
 variable "cluster_version" {
   description = "Kubernetes version to use for the EKS cluster."
   type        = string
+  default     = 1.20
 }
 
 variable "vault_user" {
@@ -43,11 +45,13 @@ variable "is_jx2" {
 variable "jx_git_url" {
   description = "URL for the Jenins X cluster git repository"
   type        = string
+  default     = "https://github.com/zachariahtimothy/jx-test-terraform-eks.git"
 }
 
 variable "jx_bot_username" {
   description = "Bot username used to interact with the Jenkins X cluster git repository"
   type        = string
+  default     = "jx-aws-bot"
 }
 
 variable "jx_bot_token" {
